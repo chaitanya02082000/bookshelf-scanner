2c6fea7f2e943d90f8490bf3e3e7a43d2465297b
chaitanya61 <chaitanya@mastersunion.org>
2026-05-06 11:31:06 +0530

    Added Auth0 basic

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,19 @@
-import {ChangeDetectionStrategy, Component} from "@angular/core";
-import {RouterOutlet} from "@angular/router";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
+
+import { inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: "app-root",
-  standalone: true,
-  template: "<router-outlet></router-outlet>",
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterOutlet],
+    selector: "app-root",
+    standalone: true,
+    templateUrl: './app.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [RouterOutlet, CommonModule,],
 })
-export class AppComponent {}
+export class AppComponent {
+
+    protected readonly window = window;
+    protected auth = inject(AuthService);
+}
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,18 @@
-import {ApplicationConfig, provideExperimentalZonelessChangeDetection} from "@angular/core";
-import {provideHttpClient} from "@angular/common/http";
-import {provideRouter} from "@angular/router";
-import {routes} from "./app.routes";
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from "@angular/core";
+import { provideHttpClient } from "@angular/common/http";
+import { provideRouter } from "@angular/router";
+import { routes } from "./app.routes";
 
+import { provideAuth0 } from '@auth0/auth0-angular';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideExperimentalZonelessChangeDetection()],
+    providers: [provideRouter(routes), provideHttpClient(), provideExperimentalZonelessChangeDetection(),
+    provideAuth0({
+        domain: "dev-kdeoxnytvveh762k.us.auth0.com",
+        clientId: "crK6gn79cUaYckp3DArilapQ5oCP2wYZ",
+        authorizationParams: {
+            redirect_uri: window.location.origin,
+        },
+    }),
+
+    ],
 };
